67fe5f08d81200ac8d04c5ce46dd59980a2bd91d
Majestyk228 <klsarah75@sfr.fr>
2022-04-01 20:32:40 +0200

    Added activity and linked pages
    Add CreateAccountActivity and linked landing_page and signup_page

---
+++ ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/CreateAccountActivity.java
@@ -0,0 +1,18 @@
+package com.example.exprimonsnousapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+}
--- ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/MainActivity.java
+++ ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/MainActivity.java
@@ -2,15 +2,44 @@ package com.example.exprimonsnousapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialButton createAccountBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.landing_page);
+        setContentView(R.layout.landing_page);
         //setContentView(R.layout.signin_page);
-        setContentView(R.layout.login_page);
+        //setContentView(R.layout.login_page);
+
+        this.createAccountBtn = (MaterialButton)findViewById(R.id.createAccountBtn);
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),CreateAccountActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
+
+    /*
+    *
+    * this.btProduit = (Button)findViewById(R.id.btProduit);
+        btProduit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(),ProduitActivity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+    * */
 }
