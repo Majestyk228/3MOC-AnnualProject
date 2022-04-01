527f4d5e8274262c2f0e29064e553127aff39127
Majestyk228 <klsarah75@sfr.fr>
2022-04-01 19:48:08 +0200

    Screens added and renamed
    Added :
    - login_page
    - select_commune_page
    
    Renamed :
    - signin_page (became signup_page)
    
    NB : select_commune_page missing selector because not included natively in android drawables ; will be created for next commit.

--- ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/MainActivity.java
+++ ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.landing_page);
-        setContentView(R.layout.signin_page);
+        //setContentView(R.layout.signin_page);
+        setContentView(R.layout.login_page);
     }
 }
