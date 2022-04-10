16990ea24a0e000a3ed8a76b6d86a81098c7df59
kicass8 <61785047+kicass8@users.noreply.github.com>
2022-04-10 12:03:08 +0200

    Added first textInputs for testing

--- GestionDeProjetFX/src/main/java/com/exprimonsnousprojet/gestiondeprojetfx/TestFX.java
+++ GestionDeProjetFX/src/main/java/com/exprimonsnousprojet/gestiondeprojetfx/TestFX.java
@@ -1,12 +1,15 @@
 package com.exprimonsnousprojet.gestiondeprojetfx;
 
 import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -18,6 +21,10 @@ public class TestFX extends Application {
     private ArrayList<Category> categories = new ArrayList<Category>();
     private User currentUser;
     private Scanner cli;
+    private String firstname;
+    private String lastname;
+    private int inputEvents = 0;
+    private int sceneNumber = 0;
 
     public static void main(String[] args) {
         launch(); //démarrage de l'interface graphique
@@ -26,34 +33,65 @@ public class TestFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 //modification de l'eshétique basique de la fenêtre principale
-        primaryStage.setTitle("Hello FX!");
-        primaryStage.setWidth(300);
-        primaryStage.setHeight(200);
+        primaryStage.setTitle("Gestion de projet");
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(400);
 //instanciation d'un label
-        Label unLabel = new Label("Nombre de clique : ");
+        Label unLabel = new Label("Welcome, please enter your firstname : ");
+
 //instanciation d'un bouton
-        Button unBouton = new Button("Cliquez-moi !");
+        Button unBouton = new Button("Next");
 //écoute du bouton
         unBouton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event)
             {
-//action à effectuer au clique
+        //action à effectuer au clique
                 if(event.getSource()==unBouton)
                 {
-                    setNbClick(getNbClick() + 1);
-                    unLabel.setText("Nombre de clique : " +
-                            getNbClick());
+                    sceneNumber++;
+                    switchScene(sceneNumber,(VBox) unBouton.getParent());
+                    unBouton.setVisible(false);
+                    unBouton.setManaged(false);
                 }
             }
-        });//layout vertical
+        });
+        unBouton.setVisible(false);
+
+//instanciation d'un textField (text input)
+        TextField inputField = new TextField("Your firstname...");
+//écoute du textField
+        inputField.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event)
+            {
+//action à effectuer au clique
+                if(event.getSource()==inputField)
+                {
+                    if (inputEvents == 0){
+                        firstname = inputField.getText().trim();
+                        unLabel.setText("Please enter your lastname : ");
+                        inputField.setText("Your lastname...");
+                        inputEvents++;
+                    }else {
+                        lastname = inputField.getText().trim();
+                        inputEvents++;
+                        inputField.setVisible(false);
+                        inputField.setManaged(false);
+                        unLabel.setText("Welcome " + firstname + " " + lastname + " !");
+                        unBouton.setVisible(true);
+                    }
+                }
+            }
+        });
+        //layout vertical
         VBox root = new VBox(25);
 //centrage des éléments de la VBox
         root.setAlignment(Pos.CENTER);
 //instanciation de la scene avec la VBox root comme parent
-        Scene uneScene = new Scene(root, 300,200);
+        Scene uneScene = new Scene(root, 400,300);
 //ajout des éléments à la vbox : le label puis le bouton en dessous
-        root.getChildren().addAll(unLabel, unBouton);
+        root.getChildren().addAll(unLabel, inputField, unBouton);
 //définition de la scène attribuée à la Stage
         primaryStage.setScene(uneScene);
 //interdiction du redimentionnement
@@ -62,14 +100,13 @@ public class TestFX extends Application {
         primaryStage.show();
 //centrage de la fenêtre au centre de l'écran
         primaryStage.centerOnScreen();
+
     }
-    /*================== getter & setter de la variable nbClick
-    ==================*/
-    public int getNbClick() {
-        return nbClick;
-    }
-    public void setNbClick(int nbClick) {
-        this.nbClick = nbClick;
+    private void switchScene(int sceneNumber, VBox root){
+        if (sceneNumber == 1){
+            System.out.println(root.getChildren());
+            System.out.println(root);
+        }
     }
 }
 
