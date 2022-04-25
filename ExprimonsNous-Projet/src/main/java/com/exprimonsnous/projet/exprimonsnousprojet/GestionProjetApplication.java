553cb3e493d1f361ff7e2590c81c3c7a788ec02c
kicass8 <kicass@live.fr>
2022-04-25 17:16:20 +0200

    Testing interaction

--- ExprimonsNous-Projet/src/main/java/com/exprimonsnous/projet/exprimonsnousprojet/GestionProjetApplication.java
+++ ExprimonsNous-Projet/src/main/java/com/exprimonsnous/projet/exprimonsnousprojet/GestionProjetApplication.java
@@ -1,8 +1,12 @@
 package com.exprimonsnous.projet.exprimonsnousprojet;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +15,39 @@ public class GestionProjetApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GestionProjetApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setTitle("Connexion");
         stage.setScene(scene);
         stage.show();
+
+        Button ConnexionButton = (Button) scene.lookup("#ConnexionButton");
+        TextField firstname = (TextField) scene.lookup("#FirstnameTextField");
+        TextField lastname = (TextField) scene.lookup("#LastnameTextField");
+        int destroyEvent = 0;
+
+        //Creating the mouse event handler
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                System.out.println("clicked");
+                if (ConnexionButton.getText().compareTo("Connected!") == 0){
+                    ConnexionButton.setVisible(false);
+                    ConnexionButton.setManaged(false);
+                } else if (firstname.getText().compareTo("Kilian") == 0 && lastname.getText().compareTo("Cassaigne") == 0){
+                    ConnexionButton.setText("Connected!");
+                } else {
+                    ConnexionButton.setText("Identifiants incorrects");
+                    FXMLLoader loader = new FXMLLoader(GestionProjetApplication.class.getResource("AppView.fxml"));
+                    Scene scene = new Scene(loader.load(), 600, 500);
+                    stage.setScene(scene);
+                }
+            }
+        };
+        //Registering the event filter
+        ConnexionButton.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
+
+
+
     }
 
     public static void main(String[] args) {
