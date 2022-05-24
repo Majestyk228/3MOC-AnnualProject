c6feac22a2dbfe8ad4e3e4c9d05bc212fb104bdf
kicass8 <61785047+kicass8@users.noreply.github.com>
2022-05-25 01:19:39 +0200

    Revamped design according to figma, Added back to home and exit button to AppView, Changed how lists and tasks were made.

--- ExprimonsNous-Projet/src/main/java/com/exprimonsnous/projet/exprimonsnousprojet/AppView.java
+++ ExprimonsNous-Projet/src/main/java/com/exprimonsnous/projet/exprimonsnousprojet/AppView.java
@@ -4,23 +4,37 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import static java.lang.Double.MAX_VALUE;
+
 public class AppView {
 
+    private static Stage stage;
+    private Scene scene;
+
+    @FXML
+    static Rectangle ReferenceRectangle;
     @FXML
     private GridPane ListPane;
     @FXML
     private Button AddListButton;
+    @FXML
+    private VBox VBOX;
 
 
 
@@ -43,9 +57,11 @@ public class AppView {
         TitledListPane.setText(listName);
         Button AddTaskButton = new Button("Ajouter une tâche");
         AddTaskButton.setWrapText(true);
+        AddTaskButton.setTextOverrun(OverrunStyle.CLIP);
         ListView content = new ListView<>();
         content.setMaxWidth(150);
         content.setPrefWidth(150);
+        content.setMaxHeight(MAX_VALUE);
         AddTaskButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -62,15 +78,22 @@ public class AppView {
             }
         });
         content.getItems().add(AddTaskButton);
+        TitledListPane.setAnimated(false);
         TitledListPane.setContent(content);
         TitledListPane.setWrapText(true);
+        TitledListPane.setTextOverrun(OverrunStyle.CLIP);
         ListPane.add(TitledListPane, GridPane.getColumnIndex(AddListButton), 0);
         ListPane.setValignment(TitledListPane, VPos.TOP);
+        ListPane.setHgrow(TitledListPane, Priority.NEVER);
+        ListPane.setVgrow(TitledListPane, Priority.ALWAYS);
+        ListPane.setVgrow(content, Priority.ALWAYS);
+        VBOX.setVgrow(TitledListPane, Priority.ALWAYS);
+        VBOX.setVgrow(content, Priority.ALWAYS);
         ColumnConstraints column = new ColumnConstraints();
         column.setMaxWidth(150);
         ListPane.getColumnConstraints().add(column);
         ListPane.setColumnIndex(AddListButton, GridPane.getColumnIndex(AddListButton) + 1);
-
+        System.out.println(ListPane.getHeight());
     }
 
     public void AddTask(String taskname, ListView parent, Button button){
@@ -79,7 +102,7 @@ public class AppView {
         TitledTaskPane.setText(taskname);
         TitledTaskPane.setContent(test);
         TitledTaskPane.setWrapText(true);
-        TitledTaskPane.setMaxWidth(140);
+        TitledTaskPane.setTextOverrun(OverrunStyle.CLIP);
         parent.getItems().remove(parent.getItems().toArray().length - 1);
         parent.getItems().add(TitledTaskPane);
         button.setAlignment(Pos.CENTER);
@@ -89,9 +112,24 @@ public class AppView {
     public void CloseListMenu(ActionEvent event) {
     }
 
-    public void SwitchToHome(ActionEvent event) {
+    public void SwitchToHome(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Home.class.getResource("MainView.fxml"));
+        stage = (Stage) (AddListButton.getParent().getScene().getWindow());
+        scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void ExitApp(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quitter");
+        alert.setHeaderText("Vous êtes sur le point de quitter l'appli !");
+        alert.setContentText("Voulez-vous vraiment fermer cette application ? Tout progrès non sauvegardé sera perdu");
+
+        if (alert.showAndWait().get() == ButtonType.OK){
+            stage = (Stage) ListPane.getScene().getWindow();
+            System.out.println("User exited the app");
+            stage.close();
+        }
     }
 }
