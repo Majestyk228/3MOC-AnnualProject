08d504292e74ab677b104219ab61e0be5876e00b
Majestyk228 <klsarah75@sfr.fr>
2022-05-28 23:07:54 +0200

    Updating post route
    need to be fixed

--- EN-Server/services/post.js
+++ EN-Server/services/post.js
@@ -5,6 +5,12 @@ async function getAllPosts() {
 	return rows;
 }
 
+
+async function getAllPostsBis() {
+	const rows = await db.query("SELECT u.firstName as firstName, u.lastName as lastName, p.title as title, p.body as body, p.likes as likes, p.dislikes as dislikes, COUNT(com.idComment) as nbComment, COUNT(c.idPost) as nbReward FROM User u, Post p, Comment com, Congratulate c WHERE com.idPost = p.idPost AND c.idPost = p.idPost AND p.idUser = u.idUser;", "");
+	return rows;
+}
+
 async function getPost(id) {
 	const rows = await db.query("SELECT * FROM Post WHERE idPost=" + id + ";", "");
 	return rows;
@@ -12,5 +18,6 @@ async function getPost(id) {
 
 module.exports = {
 	getAllPosts,
+	getAllPostsBis,
 	getPost
 }
