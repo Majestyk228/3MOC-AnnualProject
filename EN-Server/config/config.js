d2ce8e00dc4aa43f025fb4795ebc514d455769fe
Majestyk228 <60360588+Majestyk228@users.noreply.github.com>
2022-06-11 18:49:04 +0200

    Update config.js

--- EN-Server/config/config.js
+++ EN-Server/config/config.js
@@ -2,7 +2,7 @@ const config = {
 	db: {
 		host: "exprimonsnous.coqdgtazlflp.us-east-1.rds.amazonaws.com",
 		user: "admin",
-		password: "kB9qG7e3zEU3",
+		password: "",
 		database: "exprimonsNous",
 		dateStrings: 'date'
 	}
