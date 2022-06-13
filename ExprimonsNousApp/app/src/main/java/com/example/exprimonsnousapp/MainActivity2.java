79dfe476433f470152ad0b42faf7b80a496dc772
Majestyk228 <klsarah75@sfr.fr>
2022-06-13 21:53:44 +0200

    Navigation functionnal
    TODO : convert the PostFeedActivity into a Fragment

--- ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/MainActivity2.java
+++ ExprimonsNousApp/app/src/main/java/com/example/exprimonsnousapp/MainActivity2.java
@@ -1,37 +1,54 @@
 package com.example.exprimonsnousapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.exprimonsnousapp.databinding.ActivityMain2Binding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private ActivityMain2Binding binding;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
 
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        bottomNavigationView = findViewById(R.id.activity_main_bottom_navigation);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.action_posts:
+                        selectedFragment = new PostFragment();
+                        break;
+                    case R.id.action_votes:
+                        selectedFragment = new VoteFragment();
+                        break;
+                    case R.id.action_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                    /*case R.id.nav_notification:
+                        selectedFragment = new NotificationFragment();
+                        break;*/
+
+                }
+
+                assert selectedFragment != null;
+                getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_frame_layout,selectedFragment).commit();
+
+                return true;
+
+            }
+        });
     }
-
 }
