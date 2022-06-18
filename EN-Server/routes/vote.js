e5dea98125addd5b5aa3280319bd8293f175bf04
Majestyk228 <klsarah75@sfr.fr>
2022-06-18 18:04:12 +0200

    Route voteInfo created
    Issue with nbChoiceVoteOptions (returns promise that i cannot await)

--- EN-Server/routes/vote.js
+++ EN-Server/routes/vote.js
@@ -18,6 +18,42 @@ router.post('/voteList', async function (req, res, next) {
 
 
 
+/* GET Vote's infos, their choices and the number of time each choices has been selected MUST BE IN BODY*/
+router.post('/voteInfo', async function (req, res, next) {
+	try {
+		//TODO : take title and body
+		const titleBody = await vote.getVoteTitleBody(req.body.idVote)
+
+		//TODO : Take choices list
+		const choiceList = await vote.getVoteOptions(req.body.idVote)
+
+		//TODO : Take number for selection for each choice
+
+		var nbSelected = []
+
+		choiceList.forEach(choice => {
+			nbSelected.push(vote.getListOfNumber(req.body.idVote, choice)).value
+		})
+
+		console.log(nbSelected)
+
+		res.status(200).json(
+			[
+				{
+					"title": titleBody[0].title,
+					"body": titleBody[0].body,
+					"voteOptions": choiceList,
+					"nbChoiceVoteOptions": nbSelected
+				}
+			]
+		);
+	} catch (err) {
+		res.status(400).json([{ "ERROR": "Bad Request" }]);
+		next(err);
+	}
+});
+
+
 
 
 module.exports = router;
--- EN-Server/services/vote.js
+++ EN-Server/services/vote.js
@@ -6,6 +6,48 @@ async function getVoteListByCommunity(idCommunity) {
 	return rows;
 }
 
+// TODO FUNCTIONS TO BUILD VOTE TEMPLATE
+// title, body from vote
+async function getVoteTitleBody(idVote) {
+	const rows = await db.query("SELECT v.title, v.body FROM Vote v WHERE v.idVote = " + idVote + ";", "");
+	return rows;
+}
+
+// label from voteOptions
+async function getVoteOptions(idVote) {
+	const rows = await db.query("SELECT vo.label, vo.idVoteOptions FROM VoteOptions vo WHERE vo.idVote = " + idVote + ";", "");
+	return rows;
+}
+
+
+//nbChoice
+async function getNbChoices(idVote, choice) {
+	const query = "SELECT COUNT(vs.choice) AS nbChoice FROM Votes vs, VoteOptions vo WHERE vs.idVote = " + idVote + " AND vs.choice = " + choice + " AND vs.choice = vo.idVoteOptions;";
+
+	//console.log(query)
+	const rows = await db.query(query, "");
+	return rows;
+}
+
+
+async function getListOfNumber(idVote, choice) {
+
+	var number = 0;
+
+	number = await getNbChoices(idVote, choice['idVoteOptions'])
+
+	return (number[0])
+}
+
+
+
+
+
+
 module.exports = {
-	getVoteListByCommunity
+	getVoteListByCommunity,
+	getVoteTitleBody,
+	getVoteOptions,
+	getNbChoices,
+	getListOfNumber
 }
