cd79c5d5cf7de9b261f066cdd593863dc8921035
tdtorres <ttorresdacosta@myges.fr>
2022-06-20 15:09:26 +0200

    init Login

---
+++ ExprimonsAdmin/lib/loginview.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
