20569e5b147c2029af1dc4a9d2ffc2200f60f641
Majestyk228 <klsarah75@sfr.fr>
2022-07-03 15:24:41 +0200

    Update Vote route

--- EN-Server/services/vote.js
+++ EN-Server/services/vote.js
@@ -16,7 +16,7 @@ var Datetime = (today + ' ' + time);
 
 
 async function getVoteListByCommunity(idCommunity) {
-	const rows = await db.query("SELECT idVote, title FROM Vote WHERE idCommunity = " + idCommunity + "ORDER BY voteBegins;", "");
+	const rows = await db.query("SELECT * FROM Vote WHERE idCommunity = " + idCommunity + ";", "");
 	return rows;
 }
 
