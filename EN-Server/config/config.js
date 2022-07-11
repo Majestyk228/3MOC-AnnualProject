dafe300572966d0c55722dc38fa3d977680955ff
Majestyk228 <klsarah75@sfr.fr>
2022-07-11 11:31:58 +0200

    setting up backup database

--- EN-Server/config/config.js
+++ EN-Server/config/config.js
@@ -1,9 +1,21 @@
-const config = {
+/*const config = {
 	db: {
 		host: "exprimonsnous.coqdgtazlflp.us-east-1.rds.amazonaws.com",
 		user: "admin",
 		password: "kB9qG7e3zEU3",
 		database: "exprimonsNous",
+		dateStrings: 'date'
+	}
+};
+module.exports = config;*/
+
+
+const config = {
+	db: {
+		host: "exprimonsnous-bkp.c50gqqod3ibz.eu-west-3.rds.amazonaws.com",
+		user: "admin",
+		password: "kB9qG7e3zEU3",
+		database: "exprimonsNous_bkp",
 		dateStrings: 'date'
 	}
 };
