3b837e0ebb4d749046a42844392a461d53f63bdc
Majestyk228 <klsarah75@sfr.fr>
2022-07-21 17:59:30 +0200

    Removing ancien project
    Restarting fresh

--- ExprimonsNous-Projet/src/main/java/com/exprimonsnous/projet/exprimonsnousprojet/TaskViewController.java
+++
@@ -1,9 +0,0 @@
-package com.exprimonsnous.projet.exprimonsnousprojet;
-
-import javafx.event.ActionEvent;
-
-public class TaskViewController {
-
-    public void login(ActionEvent event) {
-    }
-}
