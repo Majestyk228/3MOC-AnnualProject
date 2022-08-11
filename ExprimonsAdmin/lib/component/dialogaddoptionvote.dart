f5ddf901f630e45f1cab530cbeea6ec4bae34e1f
tdtorres <92336506+tdtorres@users.noreply.github.com>
2022-08-11 20:31:08 +0200

    dialog options votes UI

--- ExprimonsAdmin/lib/component/dialogaddoptionvote.dart
+++ ExprimonsAdmin/lib/component/dialogaddoptionvote.dart
@@ -35,113 +35,148 @@ class _DialogAddOptionVoteState extends State<DialogAddOptionVote> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      backgroundColor: Colors.transparent,
       child: Container(
-        width: 700,
-        height: 300,
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Container(
-                width: 1000,
-                height: 200,
-                child: Row(
-                  children: [
-                    Center(
-                      child: Expanded(
-                        child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            shrinkWrap: true,
-                            addRepaintBoundaries: true,
-                            itemCount: widget.nbChoice,
-                            itemBuilder: (context, index) {
-                              return Card(
-                                shadowColor: DarkRedColor,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                elevation: 25,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        width: 150,
-                                        height: 100,
-                                        child: TextField(
-                                          style: InputStyle,
-                                          minLines: 1,
-                                          maxLines: 5,
-                                          onChanged: (text) {
-                                            optionVotes[index].label = text;
-                                          },
-                                          autocorrect: true,
-                                          decoration: InputDecoration(
-                                            focusedBorder: OutlineInputBorder(
-                                                borderSide: BorderSide(
-                                                    color: DarkRedColor,
-                                                    width: 2)),
-                                            border: OutlineInputBorder(
-                                              borderSide: const BorderSide(
-                                                  color: Color(0xFFFFCBD0),
-                                                  width: 3.0),
-                                            ),
-                                            labelText: 'option ${index + 1} ',
-                                            floatingLabelStyle:
-                                                TextStyle(color: Colors.red),
-                                            /*
-                                            errorText: _validateBody
-                                                ? 'Cette valeur ne peut etre vide'
-                                                : null,
+        width: 1000,
+        height: 500,
+        child: Card(
+          color: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Container(
+                    width: 1000,
+                    height: 100,
 
-                                             */
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-                            }),
+                    //child: Text(votes[index]["title"])
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)),
+                      elevation: 2,
+                      color: DarkRedColor,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Text(
+                              "Ajout d'utilisateur",
+                              style: TitleAddStyle,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 400,
-                height: 75,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                    color: DarkRedColor,
-                    elevation: 5,
-                    child: TextButton(
-                        onPressed: () async {
-                          final idVote = await addVotes(
-                              widget.title,
-                              widget.body,
-                              "${widget.nbChoice}",
-                              widget.important);
-
-                          for (int i = 0; i < optionVotes.length; i++) {
-                            print(optionVotes[i].label);
-                            optionVotes[i].idVote = idVote;
-                          }
-                          addOptionVotes(optionVotes);
-                          Navigator.pop(context);
-                          Navigator.pop(context);
-                        },
-                        child: Text(
-                          "Créer les options du vote",
-                          style: RedButtonStyle,
-                        )),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 1000,
+                  height: 200,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Expanded(
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              addRepaintBoundaries: true,
+                              itemCount: widget.nbChoice,
+                              itemBuilder: (context, index) {
+                                return Card(
+                                  shadowColor: DarkRedColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  elevation: 25,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          width: 200,
+                                          height: 100,
+                                          child: TextField(
+                                            style: InputStyle,
+                                            minLines: 1,
+                                            maxLines: 5,
+                                            onChanged: (text) {
+                                              optionVotes[index].label = text;
+                                            },
+                                            autocorrect: true,
+                                            decoration: InputDecoration(
+                                              focusedBorder: OutlineInputBorder(
+                                                  borderSide: BorderSide(
+                                                      color: DarkRedColor,
+                                                      width: 2)),
+                                              border: OutlineInputBorder(
+                                                borderSide: const BorderSide(
+                                                    color: Color(0xFFFFCBD0),
+                                                    width: 3.0),
+                                              ),
+                                              labelText: 'option ${index + 1} ',
+                                              floatingLabelStyle:
+                                                  TextStyle(color: Colors.red),
+                                              /*
+                                              errorText: _validateBody
+                                                  ? 'Cette valeur ne peut etre vide'
+                                                  : null,
+
+                                               */
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 400,
+                  height: 75,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)),
+                      color: DarkRedColor,
+                      elevation: 5,
+                      child: TextButton(
+                          onPressed: () async {
+                            final idVote = await addVotes(
+                                widget.title,
+                                widget.body,
+                                "${widget.nbChoice}",
+                                widget.important);
+
+                            for (int i = 0; i < optionVotes.length; i++) {
+                              print(optionVotes[i].label);
+                              optionVotes[i].idVote = idVote;
+                            }
+                            addOptionVotes(optionVotes);
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Créer les options du vote",
+                            style: RedButtonStyle,
+                          )),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
