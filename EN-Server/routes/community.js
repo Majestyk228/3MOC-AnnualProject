c912d295163211d312af998fc73dd4fec3f9ac1c
Majestyk228 <klsarah75@sfr.fr>
2022-08-19 22:54:10 +0200

    correcting community stats

--- EN-Server/routes/community.js
+++ EN-Server/routes/community.js
@@ -33,8 +33,8 @@ router.post('/bestUsers', async function (req, res, next) {
 	}
 });
 
-
-router.post('/stats', async function (req, res, next) {
+//TODO : Corriger les données
+router.post('/stats2', async function (req, res, next) {
 	try {
 		if (req.headers.token) {
 			// VERIFY TOKEN
@@ -48,6 +48,44 @@ router.post('/stats', async function (req, res, next) {
 				} else {
 					res.status(200).json(result);
 				}
+			} catch (err) {
+				// IF TOKEN IS INVALID
+				res.status(406).json([{ "ERROR": "Token expired/incorrect" }]);
+			}
+		} else {
+			res.status(404).json([{ "ERROR": "Missing token in header" }]);
+		}
+	} catch (err) {
+		res.status(400).json([{ "ERROR": err.message }]);
+		next(err);
+	}
+});
+
+
+router.post('/stats', async function (req, res, next) {
+	try {
+		if (req.headers.token) {
+			// VERIFY TOKEN
+			try {
+				// IF TOKEN IS VALID
+				const decoded = jwt.verify(req.headers.token, config.JWT_SIGN_SECRET)
+
+				const nbUsers = await community.getNbUsers(req.body.idCommunity);
+
+				const totalPointsCommunity = await community.getTotalPointsCommunity(req.body.idCommunity);
+
+				const nbPosts = await community.getNbPosts(req.body.idCommunity);
+
+				const nbVotes = await community.getNbVotes(req.body.idCommunity);
+
+				// TODO : BUILD RESPONSE
+				res.status(200).json([{
+					"nbUsers": nbUsers.nbUsers,
+					"totalPointsCommunity": parseInt(totalPointsCommunity.totalPointsCommunity),
+					"nbPosts": nbPosts.nbPost,
+					"nbVotes": nbVotes.nbVote,
+				}]);
+
 			} catch (err) {
 				// IF TOKEN IS INVALID
 				res.status(406).json([{ "ERROR": "Token expired/incorrect" }]);
--- EN-Server/services/community.js
+++ EN-Server/services/community.js
@@ -30,10 +30,69 @@ async function getThreeBestUserByCommunity(id) {
 }
 
 
+
+
+
+
+
+
+
+/*
+//#1
+//SELECT COUNT(a.idUser) as nbUsers FROM Associate a WHERE a.idCommunity = 2;
+const nbUsers = await community.getNbUsers(req.body.idCommunity);
+
+//#2
+//SELECT SUM(u.points) as totalPointsCommunity FROM User u, Associate a WHERE a.idCommunity = 2 AND u.idUser = a.idUser;
+const totalPointsCommunity = await community.getTotalPointsCommunity(req.body.idCommunity);
+
+//#3
+//SELECT COUNT(idPost) as nbPost FROM Post WHERE idCommunity = 2;
+const nbPosts = await community.getNbPosts(req.body.idCommunity);
+
+//#4
+//SELECT COUNT(idVote) as nbVote FROM Vote WHERE idCommunity = 2;
+const nbVotes = await community.getNbVotes(req.body.idCommunity);
+*/
+async function getNbUsers(idCommunity) {
+	const request = "SELECT COUNT(a.idUser) as nbUsers FROM Associate a WHERE a.idCommunity = " + idCommunity + ";";
+	const rows = await db.query(request, "");
+	return rows[0];
+}
+
+
+
+async function getTotalPointsCommunity(idCommunity) {
+	const request = "SELECT SUM(u.points) as totalPointsCommunity FROM User u, Associate a WHERE a.idCommunity = " + idCommunity + " AND u.idUser = a.idUser;";
+	const rows = await db.query(request, "");
+	return rows[0];
+}
+
+
+
+async function getNbPosts(idCommunity) {
+	const request = "SELECT COUNT(idPost) as nbPost FROM Post WHERE idCommunity = " + idCommunity + ";";
+	const rows = await db.query(request, "");
+	return rows[0];
+}
+
+
+
+async function getNbVotes(idCommunity) {
+	const request = "SELECT COUNT(idVote) as nbVote FROM Vote WHERE idCommunity = " + idCommunity + ";";
+	const rows = await db.query(request, "");
+	return rows[0];
+}
+
+
 module.exports = {
 	getCommunityInfo,
 	getCommunityStats,
 	getTopChoicesVotes,
 	getUsersCommunity,
-	getThreeBestUserByCommunity
+	getThreeBestUserByCommunity,
+	getNbUsers,
+	getTotalPointsCommunity,
+	getNbPosts,
+	getNbVotes
 }
