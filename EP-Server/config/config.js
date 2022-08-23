b5d6396f1df3f4897c6674ab9044625b05cc5b47
Majestyk228 <klsarah75@sfr.fr>
2022-08-23 21:06:57 +0200

    correcting config

--- EP-Server/config/config.js
+++ EP-Server/config/config.js
@@ -1,6 +1,7 @@
 const config = {
     db: {
-        host: "database-1.c50gqqod3ibz.eu-west-3.rds.amazonaws.com",
+        //host: "database-1.c50gqqod3ibz.eu-west-3.rds.amazonaws.com",
+        host: "exprimons-projet.c50gqqod3ibz.eu-west-3.rds.amazonaws.com",
         user: "admin",
         password: "kB9qG7e3zEU3",
         database: "exprimonsnousprojet",
